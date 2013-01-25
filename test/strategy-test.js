95a53b820374e767b04b1aade9b45f7091234d89
Jared Hanson <jaredhanson@gmail.com>
2013-01-24 22:06:20 -0800

    Wrap error for more informative message.

--- test/strategy-test.js
+++ test/strategy-test.js
@@ -161,6 +161,9 @@ vows.describe('NetflixStrategy').addBatch({
       'should error' : function(err, req) {
         assert.isNotNull(err);
       },
+      'should wrap error in InternalOAuthError' : function(err, req) {
+        assert.equal(err.constructor.name, 'InternalOAuthError');
+      },
       'should not load profile' : function(err, profile) {
         assert.isUndefined(profile);
       },
